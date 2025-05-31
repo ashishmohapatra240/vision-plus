293231d9beb6f866245c94cbc9112093b9a3ded4
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-05-31 12:36:24 +0530

    fix: update button font weight from bold to medium

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         className={cn(
-          "inline-flex items-center justify-center gap-2 rounded-[88px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
+          "inline-flex items-center justify-center gap-2 rounded-[88px] font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer",
           {
             "bg-pink-600 text-white hover:bg-pink-700": variant === "primary",
             "bg-transparent  border border-gray-300":
